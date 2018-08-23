32cda58322589d0f315ef388daaa1f0fe85d32cc
Romane <romane71193@gmail.com>
2018-08-23 13:49:48 -0400

    add crypto lookup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,30 @@
 class HomeController < ApplicationController
   def index
+    # require 'net/http'
+    # require 'json'
+
+
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = ['BTC','XRP','ADA','STEEM']
+
   end
+def lookup
+@symbol = params[:search]
+if @symbol
+@symbol =  @symbol.upcase
+if @symbol != ''&& @symbol.size > 4
+@symbol = "Not a valid Currency!"
+end
+end
+  if @symbol == ''
+  @symbol = "Hey you forgot to enter a Currency!"
+  end
+
+end
+
 
   def about
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get '/about' ,to: 'home#about'
+  get 'home/about' ,to: 'home#about'
+  get 'home/lookup', to: 'home#lookup'
+  post 'home/lookup',to: 'home#lookup'
+
   resources :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
