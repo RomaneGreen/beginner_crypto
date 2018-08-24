3ba7011873eb03a90446eb51385bace5aa410776
Romane <romane71193@gmail.com>
2018-08-23 21:37:12 -0400

    tweaked show page

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -19,6 +19,14 @@ class CryptosController < ApplicationController
   # GET /cryptos/1
   # GET /cryptos/1.json
   def show
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @cryptos = Crypto.all
+    @show_crypto = JSON.parse(@response)
   end
 
   # GET /cryptos/new
