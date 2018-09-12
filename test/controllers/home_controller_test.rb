5110701ae25d8241e146038f3ba9343b8c23c807
Romane <romane71193@gmail.com>
2018-09-12 09:47:07 -0400

    basic test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get about pg" do
+    get home_about_url
+    assert_response :success
+  end
   test "should get index" do
     get home_index_url
+    assert_response :success
+  end
+  test "should get lookup pg" do
+    get home_lookup_url
     assert_response :success
   end
 
